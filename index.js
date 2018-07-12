8bb8016408b5eb1e2d862a0b60f22f60064b01c8
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 18:28:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ function nestedTarget(){
   return document.querySelector('#nested .target');
 }
 function increaseRankBy(n){
-const rankedLists = document.querySelectorAll('.ranked-list');
+const rankedLists = document.querySelectorAll('.ranked-list')
 
-for (let i = 0, l = children.length; i < l; i++) {
+for (let i = 0, l = rankedLists.length; i < l; i++) {
   let children = rankedLists[i].children
-}
+
 for (let j = 0, k = children.length; j < k; j++) {
   children[j].innerHTML = parseInt(children[j].innerHTML) + n
 }
-
+}
 }
 function deepestChild(){
   return document.querySelector('#grand-node div div div div')
