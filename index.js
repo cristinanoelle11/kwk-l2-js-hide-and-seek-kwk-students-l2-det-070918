6cfc645c0c55dc21e0ec11796ba724f77970845f
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 18:18:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 //Code your solution here
 function getFirstSelector(selector){
-  return document.querySelector(selector)
+  return document.querySelector(selector);
 }
 function nestedTarget(){
-  return document.querySelector('#nested .target')
+  return document.querySelector('#nested .target');
 }
-function increaseRankBy(3){
-  const rankedLists = document.querySelectorAll('.ranked-list')
-const firstList = rankedLists[0]
-const secondList = rankedLists[1]
-let children = firstList.children
-let start = 1
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list');
+const firstList = rankedLists[0];
+const secondList = rankedLists[1];
+let children = firstList.children;
+let start = 1;
 for (let i = 0, l = children.length; i < l; i++) {
-  expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
+  expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3);
 }
-children = secondList.children
-start = 12
+children = secondList.children;s
+start = 12;
 for (let i = 0, l = children.length; i < l; i++) {
-  expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3)
+  expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3);
+}
 }
