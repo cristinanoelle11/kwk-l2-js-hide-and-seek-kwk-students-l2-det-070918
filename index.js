1bbe468c6f843c102be74a12d6a1f8ef340ae7ff
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 18:21:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector('#nested .target');
 }
-function increaseRankBy(){
+function increaseRankBy(n){
   const rankedLists = document.querySelectorAll('.ranked-list');
 const firstList = rankedLists[0];
 const secondList = rankedLists[1];
