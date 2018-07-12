22b514953565271e6d101bf7546cb32c3a2ed6ed
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 18:13:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,17 @@ function nestedTarget(){
   return document.querySelector('#nested .target')
 }
 function increaseRankBy(n){
-  
+  const rankedLists = document.querySelectorAll('.ranked-list')
+const firstList = rankedLists[0]
+const secondList = rankedLists[1]
+let children = firstList.children
+let start = 1
+for (let i = 0, l = children.length; i < l; i++) {
+  expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
+}
+children = secondList.children
+start = 12
+for (let i = 0, l = children.length; i < l; i++) {
+  expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3)
+}
 }
